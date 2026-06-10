3ac4cea18814fca9d390c76d6e69b5c037ff6d6d
Sasha <alexyartsev2015@gmail.com>
2026-06-10 20:58:05 +0300

    Add files via upload
    I just uploaded my code, whats so deep?

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+long number = 1; // Start at 1
+
+while (true)
+{
+    long steps = 0;   
+    long a = number;  
+    
+    while (a != 1)
+    {
+        if (a % 2 == 0)
+        {
+            a = a / 2;
+        }
+        else
+        {
+            a = 3 * a + 1;
+        }
+        steps++;
+    }
+    
+    // Print the original starting number and its steps
+    Console.WriteLine($"Number: {number}, Steps: {steps}");
+    
+    number++; 
+}
